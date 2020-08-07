cd8baedb67b57238e412e3dd15e32ee8a5fd9fdf
Mr-G00D <60680315+Mr-G00D@users.noreply.github.com>
2020-08-07 14:22:11 +0530

    Add files via upload

---
+++ ngrok.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+arch=$(dpkg --print-architecture)
+update="apt update -y >/dev/null"
+upgrade="apt upgrade -y >/dev/null"
+iwget="apt install wget zip unzip figlet -y"
+require(){
+	eval $update
+	eval $upgrade
+	eval $iwget
+}
+echo -e """\033[93m
+SIT BACK... I'LL DO THE REST
+"""
+sleep 5
+
+echo -e "\033[92m[•] \033[93mInstalling the requirements... "
+require
+echo -e "\033[92m"
+figlet "Installing Ngrok"
+echo -e "\033[95m"
+figlet "Mr.GOOD"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+chmod 777 ngrok
+mv ngrok /$HOME
+rm -rf ngrok-stable-linux-arm.zip
+echo -e "\033[92m"
+echo "[*][success]  ngrok has been installed successfully"
+echo "[*]Type  ' cd $HOME && ls ' to see the file"
+echo "[*]B Y E"
